77f5ecd8800bc016f2495a681d3ffa11a4bca974
Richard Brookman <65540488+rgbrookman@users.noreply.github.com>
2022-08-10 16:22:38 +0100

    Update ViewYearScreen.js

--- src/screens/Year/ViewYearScreen.js
+++ src/screens/Year/ViewYearScreen.js
@@ -318,7 +318,7 @@ Learn About Your Year Page
 
       <motion.div
         className="dobBox">
-          <span>I was born on...</span>
+          <span>I was born on...{day1}</span>
             <motion.div className="outputButtons">
         
                 <div>
