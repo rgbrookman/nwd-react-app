75c4edc3e7b86e6c4bdda817d1ca9f958df67542
Richard Brookman <65540488+rgbrookman@users.noreply.github.com>
2022-08-11 16:26:42 +0100

    Add files via upload

---
+++ src/components/Charts/TasksAttempted/TasksAttemptedTrend.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from 'react-router-dom';
+import { listDays } from '../../../actions/dayActions';
+import * as d3 from 'd3';
+import './ta.css';
+
+const TasksAttemptedAverageTrend = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+	const d3Chart2 = useRef();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const dayList = useSelector((state) => state.dayList);
+  const { loading, error, days } = dayList;
+
+	const parseDate = d3.timeParse('%Y-%m-%d')
+
+
+
+
+  //Data Transformations
+  days && days.forEach(day => {
+    let count = (day.momentOneScore / day.momentOneScore === 1 ? 1 : 0) + (day.momentTwoScore / day.momentTwoScore === 1 ? 1 : 0) + (day.momentThreeScore / day.momentThreeScore === 1 ? 1 : 0) + (day.momentFourScore / day.momentFourScore === 1 ? 1 : 0) + (day.momentFiveScore / day.momentFiveScore === 1 ? 1 : 0);
+    day.tasksAttempted = count;
+  });
+
+  //Getting Dates
+    const tasksAttemptedData = days && days.map(each => {
+      return {date: parseDate(each.logDate.slice(0,10)), ta: each.tasksAttempted}});
+        console.log(tasksAttemptedData);
+
+    const taDataFinal = tasksAttemptedData.sort((a,b) => a.date - b.date);
+
+
+const avgArray = [];
+
+taDataFinal && taDataFinal.map(each => avgArray.push(each.ta));
+
+
+const zeroRemove = () => {
+  let lastItem = avgArray.length - 1;
+
+  if (avgArray[lastItem] === 0) {
+    return avgArray.pop();
+  } else {
+    return avgArray;
+  }
+}
+
+zeroRemove();
+
+const recentView = avgArray.length - 1 === 0 ? avgArray.slice(avgArray.length - 14) :  avgArray.slice(-15, -1) 
+
+
+
+    const avg = Math.round(d3.mean(avgArray) * 10) / 10;
+    const recent = Math.round(d3.mean(recentView) * 10) / 10;
+
+    const newIndex = Number(recent / avg - 1).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2}); 
+
+
+	return (
+		<span className="statSpan">{newIndex}</span>
+	)
+}
+
+export default TasksAttemptedAverageTrend;
