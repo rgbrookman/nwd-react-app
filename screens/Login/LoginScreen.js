b93259a68e8c3ff58192ffb73ac93eb253a62a45
Richard Brookman <65540488+rgbrookman@users.noreply.github.com>
2022-09-13 22:12:17 +0100

    Add files via upload

---
+++ screens/Login/LoginScreen.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import Login from '../../components/Login/Login';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import './login.css';
+
+export default function LoginScreen() {
+
+  useEffect(() => {
+    document.title = "Login";
+  }, []);
+
+  return (
+    <main id="login-screen" className="loginPageScreen">
+
+     <h1 className="loginHeader">Login.</h1>
+    <Login />
+    <span>If you haven't already registered, click <a href="/register">here...</a></span>
+
+    </main>
+  );
+}
