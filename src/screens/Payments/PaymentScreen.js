d21a9310820a083ff518d8e7d7344e9b01090ced
Richard Brookman <65540488+rgbrookman@users.noreply.github.com>
2022-10-02 17:17:23 +0100

    Add files via upload

---
+++ src/screens/Payments/PaymentScreen.js
@@ -0,0 +1,132 @@
+import React from 'react';
+import './paymentscreen.css';
+import Header from "../../components/Header/Header";
+
+export default function PaymentScreen() {
+
+
+  return (
+    <>
+    <>
+    <Header />
+    </>
+    <main className="payment">
+ 
+   <section className="col1">
+    <div className="topRow">
+        <h1>I use a Pay-As-You-Can model in order to include everyone yet also generate revenue that will help NWD's reach more and more people.</h1>
+        <hr />
+        <span>Each option is valid, available and I will never deviate from this model. 
+        <hr />
+          Obviously, paying a bit more might help pay for access for someone else as well as yourself, 
+          and paying a little bit will likely at least help cover the cost of your access. 
+          But if you can't find the money, I 100% understand and never want to turn you away. 
+          <hr />
+          I know what it is like to fund the last few days of a month before payday on a some random change I forgot I put in a pocket and the joy of that accident versus the prospect of not eating. Even a £1 means something</span>
+    </div>
+   
+
+   </section>
+   <section className="paymentGrid col2">
+   <a className="gridBlock free" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=5772336" 
+target="_blank">
+        <h2>Free</h2>
+        <span>A very valid and lovely tier :)</span>
+    </a>
+    <a className="gridBlock former" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=5772336" 
+target="_blank">
+    <h2>£0.79</h2>
+<span>Patreon doesn't allow subscriptions below £1, but when I have a stripe integrated system, there will be a sub-£1 tier.</span>
+</a>
+<a 
+className="gridBlock" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=5772336" 
+target="_blank">
+<h2>£1.20</h2>
+
+</a>
+
+<a className="gridBlock" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=5772336" 
+target="_blank">
+<h2>£1.49</h2>
+
+</a>
+
+<a className="gridBlock" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=6044426" 
+target="_blank">
+<h2>£2.49</h2>
+
+</a>
+
+<a className="gridBlock" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=6044435" 
+target="_blank">
+<h2>£3.00</h2>
+
+</a>
+
+<a className="gridBlock" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=9142294" 
+target="_blank">
+<h2>£4.00</h2>
+
+</a>
+
+<a className="gridBlock" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=6044447" 
+target="_blank">
+<h2>£4.99</h2>
+</a>
+
+<a className="gridBlock" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=6044452" 
+target="_blank">
+<h2>£6.99</h2>
+</a>
+
+<a className="gridBlock" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=9142277" 
+target="_blank">
+<h2>£7.00</h2>
+</a>
+
+<a className="gridBlock" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=9142278" 
+target="_blank">
+<h2>£8.00</h2>
+</a>
+
+<a className="gridBlock" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=9142280" 
+target="_blank">
+<h2>£8.99</h2>
+</a>
+
+<a className="gridBlock" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=9142282" 
+target="_blank">
+<h2>£10.00</h2>
+</a>
+
+<a className="gridBlock" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=9142284" 
+target="_blank">
+<h2>£11.00</h2>
+</a>
+
+<a className="gridBlock" 
+href="https://www.patreon.com/join/nowasteddays/checkout?rid=9142285" 
+target="_blank">
+<h2>£12.00</h2>
+</a>
+    </section>
+    
+
+    </main>
+    </>
+  );
+}
